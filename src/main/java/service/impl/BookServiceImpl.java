ac387baf5803d5bca74463945bb2a9cb7a9fa844
Egor <llikojia162@gmail.com>
2025-07-23 16:21:41 +0200

    add BookServiceImpl

---
+++ src/main/java/service/impl/BookServiceImpl.java
@@ -0,0 +1,17 @@
+package service.impl;
+
+import java.util.List;
+import model.Book;
+import service.BookService;
+
+public class BookServiceImpl implements BookService {
+    @Override
+    public Book save(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return List.of();
+    }
+}
