e2ec892a545acc4b03812a5d70dc41c850fdfca8
Egor <llikojia162@gmail.com>
2025-07-23 10:44:38 +0200

    Create a new repository of the project with spring boot

---
+++ src/main/java/com/book_repo/book_repo/BookRepoApplication.java
@@ -0,0 +1,13 @@
+package com.book_repo.book_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRepoApplication.class, args);
+	}
+
+}
