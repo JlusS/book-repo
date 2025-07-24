d5447b9b52394691fbff32739a8a91d43849b65a
Egor <llikojia162@gmail.com>
2025-07-24 15:24:26 +0200

    CI test

--- src/main/java/bookrepo/BookRepoApplication.java
+++ src/main/java/bookrepo/BookRepoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import service.BookService;
-import service.impl.BookServiceImpl;
 
 @SpringBootApplication
 public class BookRepoApplication {
@@ -20,20 +19,20 @@ public class BookRepoApplication {
         book.setIsbn("isbn");
         book.setPrice(new BigDecimal(232424));
         book.setTitle("Yeet");
-        Book book1 = new Book();
-        book.setAuthor("TestBookAuthor1");
-        book.setCoverImage("JPEG_is_bad_idea1");
-        book.setDescription("idk1");
-        book.setIsbn("isbn1");
-        book.setPrice(new BigDecimal(2324248));
-        book.setTitle("Yeet1");
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Book book1 = new Book();
+        book1.setAuthor("TestBookAuthor1");
+        book1.setCoverImage("JPEG_is_bad_idea1");
+        book1.setDescription("idk1");
+        book1.setIsbn("isbn1");
+        book1.setPrice(new BigDecimal(2324248));
+        book1.setTitle("Yeet1");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
         BookService bookService = context.getBean(BookService.class);
 
         bookService.save(book);
         bookService.save(book1);
-
-
     }
 }
