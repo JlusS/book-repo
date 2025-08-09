712aa55f062b5da8e0fbc77d9d11b3e8f017b2f3
Egor <llikojia162@gmail.com>
2025-08-09 17:41:06 +0200

    Added constants for literals

--- src/main/java/bookrepo/repository/book/BookSpecificationBuilder.java
+++ src/main/java/bookrepo/repository/book/BookSpecificationBuilder.java
@@ -11,6 +11,12 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
+    private final static String AUTHOR = "author";
+    private final static String TITLE = "title";
+    private final static String ISBN = "isbn";
+    private final static String DESCRIPTION = "description";
+    private final static String COVER_IMAGE = "coverImage";
+    private final static String PRICE = "price";
     private final SpecificationProviderManager<Book> bookSpecificationProviderManager;
 
     @Override
@@ -18,36 +24,34 @@ public class BookSpecificationBuilder implements SpecificationBuilder<Book> {
         Specification<Book> spec = (root, query, criteriaBuilder) -> null;
 
         if (searchParameters.authors() != null && searchParameters.authors().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("author")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(AUTHOR)
                     .getSpecification(searchParameters.authors()));
         }
 
         if (searchParameters.titles() != null && searchParameters.titles().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("title")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(TITLE)
                     .getSpecification(searchParameters.titles()));
         }
 
         if (searchParameters.isbns() != null && searchParameters.isbns().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("isbn")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(ISBN)
                     .getSpecification(searchParameters.isbns()));
         }
 
         if (searchParameters.descriptions() != null && searchParameters.descriptions().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("description")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(DESCRIPTION)
                     .getSpecification(searchParameters.descriptions()));
         }
 
         if (searchParameters.coverImages() != null && searchParameters.coverImages().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("coverImage")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(COVER_IMAGE)
                     .getSpecification(searchParameters.coverImages()));
         }
 
         if (searchParameters.prices() != null && searchParameters.prices().length > 0) {
-            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider("price")
+            spec = spec.and(bookSpecificationProviderManager.getSpecificationProvider(PRICE)
                     .getSpecification(searchParameters.prices()));
         }
-
         return spec;
     }
-
 }
--- src/main/java/bookrepo/repository/book/spec/AuthorSpecificationProvider.java
+++ src/main/java/bookrepo/repository/book/spec/AuthorSpecificationProvider.java
@@ -7,14 +7,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorSpecificationProvider implements SpecificationProvider<Book> {
+    private static final String AUTHOR_PARAM = "author";
+
     @Override
     public String getKey() {
-        return "author";
+        return AUTHOR_PARAM;
     }
 
     @Override
     public Specification<Book> getSpecification(String[] params) {
         return (root, query, criteriaBuilder) ->
-                root.get("author").in((Object[]) params);
+                root.get(AUTHOR_PARAM).in((Object[]) params);
     }
 }
