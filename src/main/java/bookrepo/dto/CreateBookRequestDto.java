3b085ff1a972c926b4f14d02763d2f280e33e5c9
Egor <llikojia162@gmail.com>
2025-08-12 16:42:40 +0200

    Add a CustomGlobalExceptionHandler class. Add meaningful validation for exisiting DTO classes.

--- src/main/java/bookrepo/dto/CreateBookRequestDto.java
+++ src/main/java/bookrepo/dto/CreateBookRequestDto.java
@@ -1,16 +1,25 @@
 package bookrepo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
+    @NotNull
     private String description;
+    @NotNull
     private String coverImage;
 }
