69f29b8857cfbfac24f32b445d619f72d8ba426e
Egor <llikojia162@gmail.com>
2025-08-12 19:34:17 +0200

    fixed all issues

--- src/main/java/bookrepo/dto/CreateBookRequestDto.java
+++ src/main/java/bookrepo/dto/CreateBookRequestDto.java
@@ -1,25 +1,24 @@
 package bookrepo.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
     @NotNull
     private String author;
     @NotNull
     private String isbn;
     @NotNull
-    @Min(0)
+    @Positive
     private BigDecimal price;
-    @NotNull
     private String description;
-    @NotNull
     private String coverImage;
 }
