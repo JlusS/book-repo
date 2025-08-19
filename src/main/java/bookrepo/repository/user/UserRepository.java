3191a5531dea69f970d1f35abc8c301e11cb4c80
Egor <llikojia162@gmail.com>
2025-08-19 12:52:43 +0200

    fixed check-style

--- src/main/java/bookrepo/repository/user/UserRepository.java
+++ src/main/java/bookrepo/repository/user/UserRepository.java
@@ -1,8 +1,6 @@
 package bookrepo.repository.user;
 
 import bookrepo.model.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
